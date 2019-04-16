9452b56bab2d2c0b0366f41e64bdfeb9322d238b
Cdake3909 <13543306092@163.com>
2019-04-16 21:56:08 +0800

    one

---
+++ src/com/cq/factory/FactoryDesign.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ * @date   2019Äê4ÔÂ16ÈÕ
+ * @description
+ */
+package com.cq.factory;
+public class FactoryDesign{
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
+}
